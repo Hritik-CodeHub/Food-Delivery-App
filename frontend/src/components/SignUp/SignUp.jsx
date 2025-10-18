b443ce8863fba4713cdb323cee00de480299b1ad
Hritik Verma <hritikverma9822@gmail.com>
2025-10-18 14:04:49 +0530

    fix error

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import "./Signup.css";
+import "./SignUp.css";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
